e8aaca0e2e6d4a3ce5fc20b81e348a5656a5b918
John Ferguson Smart <john.smart@wakaleo.com>
2024-09-29 17:33:51 +0100

    Sample solution

--- src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
+++ src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
@@ -1,23 +1,42 @@
 package com.serenitydojo.playwright;
 
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.junit.UsePlaywright;
+import com.microsoft.playwright.junit.Options;
+import com.microsoft.playwright.junit.OptionsFactory;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-@UsePlaywright
+import java.util.Arrays;
+
+@UsePlaywright(ASimplePlaywrightTest.CustomOptions.class)
 public class ASimplePlaywrightTest {
+
+    public static class CustomOptions implements OptionsFactory {
+        @Override
+        public Options getOptions() {
+            return new Options()
+//                    .setHeadless(false)
+                    .setLaunchOptions(
+                            new BrowserType.LaunchOptions()
+                                    .setArgs(Arrays.asList("--no-sandbox",
+                                            "--disable-gpu",
+                                            "--disable-extensions"))
+                    );
+        }
+    }
 
     @Test
     void shouldShowThePageTitle(Page page) {
         page.navigate("https://practicesoftwaretesting.com");
         String title = page.title();
-
         Assertions.assertTrue(title.contains("Practice Software Testing"));
     }
 
     @Test
-    void shouldShowSearchTermsInTheTitle(Page page) {
+    void shouldShowSearchTermsInTheTitle1(Page page) {
         page.navigate("https://practicesoftwaretesting.com");
         page.locator("[placeholder=Search]").fill("Pliers");
         page.locator("button:has-text('Search')").click();
