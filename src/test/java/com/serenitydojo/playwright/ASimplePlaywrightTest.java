96fcac3e26f6d35e385d85e50804fece2c256fb2
John Ferguson Smart <john.smart@wakaleo.com>
2024-09-29 12:01:09 +0100

    Sample code for module 4

--- src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
+++ src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
@@ -3,6 +3,7 @@ package com.serenitydojo.playwright;
 import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.impl.AssertionsTimeout;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -18,6 +19,24 @@ public class ASimplePlaywrightTest {
         String title = page.title();
 
         Assertions.assertTrue(title.contains("Practice Software Testing"));
+
+        browser.close();
+        playwright.close();
+    }
+
+    @Test
+    void shouldShowSearchTermsInTheTitle() {
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch();
+        Page page = browser.newPage();
+
+        page.navigate("https://practicesoftwaretesting.com");
+        page.locator("[placeholder=Search]").fill("Pliers");
+        page.locator("button:has-text('Search')").click();
+
+        int matchingProductCount = page.locator(".card-title").count();
+
+        Assertions.assertTrue(matchingProductCount > 0);
 
         browser.close();
         playwright.close();
