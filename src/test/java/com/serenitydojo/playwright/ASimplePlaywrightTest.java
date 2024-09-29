667b7fa57161acd02db9f75bc273906d5b0707e0
John Ferguson Smart <john.smart@wakaleo.com>
2024-09-29 12:08:07 +0100

    Added github actions

--- src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
+++ src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
@@ -1,35 +1,23 @@
 package com.serenitydojo.playwright;
 
-import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.Page;
-import com.microsoft.playwright.Playwright;
-import com.microsoft.playwright.impl.AssertionsTimeout;
+import com.microsoft.playwright.junit.UsePlaywright;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+@UsePlaywright
 public class ASimplePlaywrightTest {
 
     @Test
-    void shouldShowThePageTitle() {
-        Playwright playwright = Playwright.create();
-        Browser browser = playwright.chromium().launch();
-        Page page = browser.newPage();
-
+    void shouldShowThePageTitle(Page page) {
         page.navigate("https://practicesoftwaretesting.com");
         String title = page.title();
 
         Assertions.assertTrue(title.contains("Practice Software Testing"));
-
-        browser.close();
-        playwright.close();
     }
 
     @Test
-    void shouldShowSearchTermsInTheTitle() {
-        Playwright playwright = Playwright.create();
-        Browser browser = playwright.chromium().launch();
-        Page page = browser.newPage();
-
+    void shouldShowSearchTermsInTheTitle(Page page) {
         page.navigate("https://practicesoftwaretesting.com");
         page.locator("[placeholder=Search]").fill("Pliers");
         page.locator("button:has-text('Search')").click();
@@ -37,8 +25,5 @@ public class ASimplePlaywrightTest {
         int matchingProductCount = page.locator(".card-title").count();
 
         Assertions.assertTrue(matchingProductCount > 0);
-
-        browser.close();
-        playwright.close();
     }
 }
