2ab2cb2f7c9768758feeaff31fd17ad1ed6d01a4
John Ferguson Smart <john.smart@wakaleo.com>
2024-10-01 09:18:25 +0100

    Sample solution

--- src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
+++ src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
@@ -3,40 +3,46 @@ package com.serenitydojo.playwright;
 import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
-import com.microsoft.playwright.junit.UsePlaywright;
-import com.microsoft.playwright.junit.Options;
-import com.microsoft.playwright.junit.OptionsFactory;
+import com.microsoft.playwright.Playwright;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
-@UsePlaywright(ASimplePlaywrightTest.CustomOptions.class)
 public class ASimplePlaywrightTest {
 
-    public static class CustomOptions implements OptionsFactory {
-        @Override
-        public Options getOptions() {
-            return new Options()
-//                    .setHeadless(false)
-                    .setLaunchOptions(
-                            new BrowserType.LaunchOptions()
-                                    .setArgs(Arrays.asList("--no-sandbox",
-                                            "--disable-gpu",
-                                            "--disable-extensions"))
-                    );
-        }
+    Playwright playwright;
+    Browser browser;
+    Page page;
+
+    @BeforeEach
+    public void setUp() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(
+                new BrowserType.LaunchOptions()
+//                        .setHeadless(false)
+                        .setArgs(Arrays.asList("--no-sandbox","--disable-extensions","--disable-gpu"))
+        );
+        page = browser.newPage();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        browser.close();
+        playwright.close();
     }
 
     @Test
-    void shouldShowThePageTitle(Page page) {
+    void shouldShowThePageTitle() {
         page.navigate("https://practicesoftwaretesting.com");
         String title = page.title();
         Assertions.assertTrue(title.contains("Practice Software Testing"));
     }
 
     @Test
-    void shouldShowSearchTermsInTheTitle1(Page page) {
+    void shouldShowSearchTermsInTheTitle() {
         page.navigate("https://practicesoftwaretesting.com");
         page.locator("[placeholder=Search]").fill("Pliers");
         page.locator("button:has-text('Search')").click();
@@ -45,4 +51,5 @@ public class ASimplePlaywrightTest {
 
         Assertions.assertTrue(matchingProductCount > 0);
     }
+
 }
