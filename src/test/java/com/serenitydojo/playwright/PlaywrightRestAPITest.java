4d294220a6ec17551f111fdafd1a9b210b939a51
John Ferguson Smart <john.smart@wakaleo.com>
2024-11-04 09:40:15 +0000

    Sample code

--- src/test/java/com/serenitydojo/playwright/PlaywrightRestAPITest.java
+++ src/test/java/com/serenitydojo/playwright/PlaywrightRestAPITest.java
@@ -1,20 +1,21 @@
 package com.serenitydojo.playwright;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import com.microsoft.playwright.*;
-import com.microsoft.playwright.options.LoadState;
+import com.microsoft.playwright.options.AriaRole;
+import com.microsoft.playwright.options.RequestOptions;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
 
 import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 @Execution(ExecutionMode.SAME_THREAD)
-public class PlaywrightWaitsTest {
+public class PlaywrightRestAPITest {
 
     protected static Playwright playwright;
     protected static Browser browser;
@@ -51,41 +52,52 @@ public class PlaywrightWaitsTest {
 
     @BeforeEach
     void openHomePage() {
+        page.route("**/products/search?q=pliers",
+                route -> route.fulfill(new Route.FulfillOptions()
+                        .setBody("{\"message\": \"Internal Server Error\"}")
+                        .setStatus(404))
+        );
         page.navigate("https://practicesoftwaretesting.com");
-        page.waitForCondition(() -> page.getByTestId("product-name").count() > 0);
     }
 
-    @DisplayName("Playwright waits automatically for elements by default")
+    @DisplayName("Playwright allows us to mock out API responses")
     @Nested
-    class AutoWaits {
+    class MockingAPIResponses {
 
         @Test
-        @DisplayName("It should wait for the filter checkbox options to appear before clicking")
-        void shouldWaitForTheFilterCheckboxes() {
-            var screwDriverFilter = page.getByLabel("Screwdriver");
-            //
-            // The checkboxes take an instant to render on the page
-            //
-            screwDriverFilter.click();
+        @DisplayName("When a search returns a single product")
+        void whenASingleItemIsFound() {
+            page.route("**/products/search?q=pliers",
+                    route -> route.fulfill(new Route.FulfillOptions()
+                            .setBody(MockSearchResponses.RESPONSE_WITH_A_SINGLE_ENTRY)
+                            .setStatus(200))
+            );
 
-            assertThat(screwDriverFilter).isChecked();
+            page.navigate("https://practicesoftwaretesting.com");
+            page.getByPlaceholder("Search").fill("pliers");
+            page.getByPlaceholder("Search").press("Enter");
+
+            assertThat(page.getByTestId("product-name")).hasCount(1);
+            assertThat(page.getByTestId("product-name")
+                    .filter(new Locator.FilterOptions().setHasText("Super Pliers")))
+                    .isVisible();
         }
 
         @Test
-        @DisplayName("It should wait for the product list to update before reading the product names")
-        void shouldWaitForProductListToUpdate() {
+        @DisplayName("When a search returns no products")
+        void whenNoItemsAreFound() {
+            page.route("**/products/search?q=pliers",
+                    route -> route.fulfill(new Route.FulfillOptions()
+                            .setBody(MockSearchResponses.RESPONSE_WITH_NO_ENTRIES)
+                            .setStatus(200))
+            );
 
-            page.getByLabel("Screwdriver").click();
-            page.getByLabel("Chisels").click();
+            page.navigate("https://practicesoftwaretesting.com");
+            page.getByPlaceholder("Search").fill("pliers");
+            page.getByPlaceholder("Search").press("Enter");
 
-            page.waitForLoadState(LoadState.LOAD);
-
-            List<String> matchingProducts = page.getByTestId("product-name").allTextContents();
-
-            Assertions.assertThat(matchingProducts)
-                    .allMatch( product -> product.contains("Screwdriver") || product.contains("Chisels"));
-
+            assertThat(page.getByTestId("product-name")).isHidden();
+            assertThat(page.getByTestId("search_completed")).hasText("There are no products found.");
         }
-
     }
 }
