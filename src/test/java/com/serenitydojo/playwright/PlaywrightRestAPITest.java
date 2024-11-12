5ce805695ead8dbb790c8e77d30f6dc257cf0e38
John Ferguson Smart <john.smart@wakaleo.com>
2024-11-12 09:34:22 +0000

    Making API calls

--- src/test/java/com/serenitydojo/playwright/PlaywrightRestAPITest.java
+++ src/test/java/com/serenitydojo/playwright/PlaywrightRestAPITest.java
@@ -1,11 +1,20 @@
 package com.serenitydojo.playwright;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import com.microsoft.playwright.*;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.stream.Stream;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
@@ -23,7 +32,7 @@ public class PlaywrightRestAPITest {
         playwright = Playwright.create();
         playwright.selectors().setTestIdAttribute("data-test");
         browser = playwright.chromium().launch(
-                new BrowserType.LaunchOptions().setHeadless(true)
+                new BrowserType.LaunchOptions().setHeadless(false)
                         .setArgs(Arrays.asList("--no-sandbox", "--disable-extensions", "--disable-gpu"))
         );
     }
@@ -87,5 +96,60 @@ public class PlaywrightRestAPITest {
             assertThat(page.getByTestId("product-name")).isHidden();
             assertThat(page.getByTestId("search_completed")).hasText("There are no products found.");
         }
+    }
+
+    @Nested
+    class MakingAPICalls {
+
+        record Product(String name, Double price) {}
+
+        private static APIRequestContext requestContext;
+
+        @BeforeAll
+        public static void setupRequestContext() {
+            requestContext = playwright.request().newContext(
+                    new APIRequest.NewContextOptions()
+                            .setBaseURL("https://api.practicesoftwaretesting.com")
+                            .setExtraHTTPHeaders(new HashMap<>() {{
+                                put("Accept", "application/json");
+                            }})
+            );
+        }
+
+        @DisplayName("Check presence of known products")
+        @ParameterizedTest(name = "Checking product {0}")
+        @MethodSource("products")
+        void checkKnownProduct(Product product) {
+            page.fill("[placeholder='Search']", product.name);
+            page.click("button:has-text('Search')");
+
+            // Check that the product appears with the correct name and price
+
+            Locator productCard = page.locator(".card")
+                    .filter(
+                        new Locator.FilterOptions()
+                                .setHasText(product.name)
+                                .setHasText(Double.toString(product.price))
+                    );
+            assertThat(productCard).isVisible();
+        }
+
+        static Stream<Product> products() {
+            APIResponse response = requestContext.get("/products?page=2");
+            Assertions.assertThat(response.status()).isEqualTo(200);
+
+            JsonObject jsonObject = new Gson().fromJson(response.text(), JsonObject.class);
+            JsonArray data = jsonObject.getAsJsonArray("data");
+
+            return data.asList().stream()
+                    .map(jsonElement -> {
+                        JsonObject productJson = jsonElement.getAsJsonObject();
+                        return new Product(
+                                productJson.get("name").getAsString(),
+                                productJson.get("price").getAsDouble()
+                        );
+                    });
+        }
+
     }
 }
