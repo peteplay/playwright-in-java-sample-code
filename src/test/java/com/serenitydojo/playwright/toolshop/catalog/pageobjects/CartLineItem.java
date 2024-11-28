2cae700d835a2a81e96dd27ea9e71c855bfd6e7a
John Ferguson Smart <john.smart@wakaleo.com>
2024-11-28 07:44:40 +0000

    Sample test suite

--- src/test/java/com/serenitydojo/playwright/toolshop/catalog/pageobjects/CartLineItem.java
+++
@@ -1,3 +0,0 @@
-package com.serenitydojo.playwright.toolshop.catalog.pageobjects;
-
-public record CartLineItem(String title, int quantity, double price, double total) {}
--- src/test/java/com/serenitydojo/playwright/toolshop/fixtures/ProductSummary.java
+++
@@ -1,3 +0,0 @@
-package com.serenitydojo.playwright.toolshop.fixtures;
-
-public record ProductSummary(String name, String price) {}
