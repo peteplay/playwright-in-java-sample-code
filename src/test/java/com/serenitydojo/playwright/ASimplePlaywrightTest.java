79011ca0bf515e009b334c276fcce5d9491e3682
peter <peter@peterharris.biz>
2025-05-13 16:27:11 +0100

    Some changes

---
+++ src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
@@ -0,0 +1,30 @@
+package com.serenitydojo.playwright;
+
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.junit.UsePlaywright;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+@UsePlaywright
+public class ASimplePlaywrightTest {
+
+    @Test
+    void shouldShowThePageTitle(Page page) {
+        page.navigate("https://practicesoftwaretesting.com");
+        String title = page.title();
+
+        Assertions.assertTrue(title.contains("Practice Software Testing"));
+    }
+
+    @Test
+    void shouldSearchByKeyword(Page page) {
+
+        page.navigate("https://practicesoftwaretesting.com");
+        page.locator("[placeholder=Search]").fill("Pliers");
+        page.locator("button:has-text('Search')").click();
+
+        int matchingSearchResults = page.locator(".card").count();
+        Assertions.assertTrue(matchingSearchResults> 0);
+    }
+}
